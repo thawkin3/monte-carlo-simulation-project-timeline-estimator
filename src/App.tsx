4e3f0ca42283685e37f141088eaa469b7c03bff9
Tyler Hawkins <thawkin3@byu.net>
2026-05-27 22:36:10 -0600

    chore: adds prettier and formats repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import { Button, Container, Flex, Heading, Theme, Text, TextArea, TextField } from "@radix-ui/themes";
-import "@radix-ui/themes/styles.css";
-import './App.css'
+import {
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Theme,
+  Text,
+  TextArea,
+  TextField,
+} from '@radix-ui/themes';
+import '@radix-ui/themes/styles.css';
+import './App.css';
 
 export const App = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
-  }
+  };
 
   return (
     <Theme>
       <main>
         <Container size="3">
           <Flex direction="column" gap="4">
-            <Heading>Monte Carlo Simulation - Project Timeline Estimator</Heading>
+            <Heading>
+              Monte Carlo Simulation - Project Timeline Estimator
+            </Heading>
             <form onSubmit={handleSubmit}>
               <Flex direction="column" gap="4">
                 <Flex direction="column" gap="2">
@@ -31,4 +42,4 @@ export const App = () => {
       </main>
     </Theme>
   );
-}
+};
