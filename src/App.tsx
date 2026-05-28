09fe08bda2147b3fbb68ce9abaf1e70b850f8818
Tyler Hawkins <thawkin3@byu.net>
2026-05-28 00:16:28 -0600

    feat: adds recharts bar chart for result data

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,72 @@ import {
   TextArea,
   TextField,
 } from '@radix-ui/themes';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid } from 'recharts';
 
 import { runMonteCarloSimulationNTimes } from './monteCarloSimulation';
 
 import '@radix-ui/themes/styles.css';
 import './App.css';
+
+const SimpleBarChart = ({ data = [] }) => {
+  const formattedDataObject = data.reduce((acc, currentValue) => {
+    if (acc[currentValue]) {
+      acc[currentValue]++;
+    } else {
+      acc[currentValue] = 1;
+    }
+    return acc;
+  }, {});
+
+  const formattedDataArray = Object.entries(formattedDataObject).map(
+    ([key, value]) => ({ daysToComplete: key, timesResultOccurred: value })
+  );
+
+  return (
+    <BarChart
+      style={{
+        width: '100%',
+        maxWidth: '700px',
+        maxHeight: '400px',
+        aspectRatio: 1.618,
+      }}
+      responsive
+      data={formattedDataArray}
+      margin={{
+        top: 5,
+        right: 0,
+        left: 0,
+        bottom: 20,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis
+        dataKey="daysToComplete"
+        label={{
+          value: 'Forecasted number of days to complete project',
+          position: 'insideBottom',
+          offset: -10,
+        }}
+      />
+      <YAxis
+        dataKey="timesResultOccurred"
+        width="auto"
+        label={{
+          value: 'Number of forecasted occurrences',
+          angle: -90,
+          position: 'middle',
+          offset: -40,
+          height: 80,
+        }}
+      />
+      <Bar
+        dataKey="timesResultOccurred"
+        fill="var(--accent-9)"
+        radius={[4, 4, 0, 0]}
+      />
+    </BarChart>
+  );
+};
 
 export const App = () => {
   const [historicalData, setHistoricalData] = React.useState('');
@@ -27,15 +88,13 @@ export const App = () => {
     const historicalDataArray = historicalData
       .split(',')
       .map((entry) => Number(entry.trim()));
-    console.log(historicalData);
-    console.log(historicalDataArray);
-    console.log(
-      runMonteCarloSimulationNTimes(
-        historicalDataArray,
-        numberOfTasksRemaining,
-        100
-      )
+    const results = runMonteCarloSimulationNTimes(
+      historicalDataArray,
+      numberOfTasksRemaining,
+      10000
     );
+
+    setSimulationResults(results);
   };
 
   return (
@@ -70,6 +129,7 @@ export const App = () => {
               </Box>
             </form>
           </Flex>
+          <SimpleBarChart data={simulationResults} />
         </Container>
       </main>
     </Theme>
