2f94279dbc0199b8fea2c441b9bb5cf444694faa
Tyler Hawkins <thawkin3@byu.net>
2026-05-27 22:32:37 -0600

    feat: creates initial monte carlo app

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { Button, Container, Flex, Heading, Theme, Text, TextArea, TextField } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
+import './App.css'
+
+export const App = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  return (
+    <Theme>
+      <main>
+        <Container size="3">
+          <Flex direction="column" gap="4">
+            <Heading>Monte Carlo Simulation - Project Timeline Estimator</Heading>
+            <form onSubmit={handleSubmit}>
+              <Flex direction="column" gap="4">
+                <Flex direction="column" gap="2">
+                  <Text as="label">Historical data</Text>
+                  <TextArea />
+                </Flex>
+                <Flex direction="column" gap="2">
+                  <Text as="label">Number of tasks remaining</Text>
+                  <TextField.Root />
+                </Flex>
+                <Button>Run simulation</Button>
+              </Flex>
+            </form>
+          </Flex>
+        </Container>
+      </main>
+    </Theme>
+  );
+}
