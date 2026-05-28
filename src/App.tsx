320ad1df5ce05f3d5091ee6188501284c429ea4d
Tyler Hawkins <thawkin3@byu.net>
2026-05-27 23:37:28 -0600

    feat: runs monte carlo simulation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import {
+  Box,
   Button,
   Container,
   Flex,
@@ -8,12 +10,32 @@ import {
   TextArea,
   TextField,
 } from '@radix-ui/themes';
+
+import { runMonteCarloSimulationNTimes } from './monteCarloSimulation';
+
 import '@radix-ui/themes/styles.css';
 import './App.css';
 
 export const App = () => {
+  const [historicalData, setHistoricalData] = React.useState('');
+  const [numberOfTasksRemaining, setNumberOfTasksRemaining] =
+    React.useState('');
+  const [simulationResults, setSimulationResults] = React.useState([]);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    const historicalDataArray = historicalData
+      .split(',')
+      .map((entry) => Number(entry.trim()));
+    console.log(historicalData);
+    console.log(historicalDataArray);
+    console.log(
+      runMonteCarloSimulationNTimes(
+        historicalDataArray,
+        numberOfTasksRemaining,
+        100
+      )
+    );
   };
 
   return (
@@ -25,17 +47,27 @@ export const App = () => {
               Monte Carlo Simulation - Project Timeline Estimator
             </Heading>
             <form onSubmit={handleSubmit}>
-              <Flex direction="column" gap="4">
-                <Flex direction="column" gap="2">
-                  <Text as="label">Historical data</Text>
-                  <TextArea />
+              <Box maxWidth="480px">
+                <Flex direction="column" gap="4">
+                  <Flex direction="column" gap="2">
+                    <Text as="label">Historical data</Text>
+                    <TextArea
+                      value={historicalData}
+                      onChange={(e) => setHistoricalData(e.target.value)}
+                    />
+                  </Flex>
+                  <Flex direction="column" gap="2">
+                    <Text as="label">Number of tasks remaining</Text>
+                    <TextField.Root
+                      value={numberOfTasksRemaining}
+                      onChange={(e) =>
+                        setNumberOfTasksRemaining(e.target.value)
+                      }
+                    />
+                  </Flex>
+                  <Button>Run simulation</Button>
                 </Flex>
-                <Flex direction="column" gap="2">
-                  <Text as="label">Number of tasks remaining</Text>
-                  <TextField.Root />
-                </Flex>
-                <Button>Run simulation</Button>
-              </Flex>
+              </Box>
             </form>
           </Flex>
         </Container>
